ef46da23aa0886a03a8444e6d2c15aa19d104d0b
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-27 12:32:45 +0200

    Make event listener and add class

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.css";
+
+let songList = [];
+
+class Song {
+    #song;
+    #length;
+
+  constructor(song, length) {
+    this.#song = song;
+    this.#length = length;
+  }
+
+  get song() {
+    return this.#song;
+  }
+
+  get length() {
+    return this.#length;
+  }
+}
+
+function addToList() {
+  var data = new Song(
+    document.getElementById("song").value,
+    document.getElementById("length").value
+  );
+  songList.push(data);
+  document.getElementById("feedback").innerText =
+    "A zene adatai hozzá lettek adva a listához";
+  setTimeout(() => {
+    document.getElementById("feedback").innerText = "";
+  }, 2000);
+}
+
+function init() {
+  document.getElementById("add").addEventListener("click", addToList);
+}
+
+document.addEventListener("DOMContentLoaded", init);
