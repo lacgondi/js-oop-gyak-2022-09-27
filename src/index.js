0e6c344d43a7df66fa5d34359a88e196af1797e7
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-09-27 12:51:34 +0200

    Add sum function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 let songList = [];
 
 class Song {
-    #song;
-    #length;
+  #song;
+  #length;
 
   constructor(song, length) {
     this.#song = song;
@@ -18,14 +18,25 @@ class Song {
   }
 }
 
+function sum() {
+  var sum = 0;
+  for (let i = 0; i < songList.length; i++) {
+    sum += songList[i]._length;
+  }
+  return sum;
+}
+
 function addToList() {
   var data = new Song(
     document.getElementById("song").value,
     document.getElementById("length").value
   );
   songList.push(data);
+  var sPlaceHolder = sum();
   document.getElementById("feedback").innerText =
     "A zene adatai hozzá lettek adva a listához";
+  document.getElementById("sum").innerText =
+    "A zeneszámok össz hossza: " + sPlaceHolder + " perc";
   setTimeout(() => {
     document.getElementById("feedback").innerText = "";
   }, 2000);
